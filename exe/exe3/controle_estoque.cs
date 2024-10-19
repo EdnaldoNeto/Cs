63bad67ee55544f01ab039c77820bcd71d94b50a
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2024-10-19 19:05:53 -0300

    update controle de estoque

--- exe/exe3/controle_estoque.cs
+++ exe/exe3/controle_estoque.cs
@@ -8,17 +8,23 @@ class Estoque{
         string[] produto = new string[100];
         string respostaUser = "";
 
+        int[] codigo = new int[100];
         int[] quantidade = new int[100];
         float[] preço = new float[100];
 
+        Console.WriteLine(produto.Length);
+        
+
         Console.WriteLine("_______________Software de controle de estoque_______________");
 
-        Console.WriteLine("\n\nOque você deseja:\n\n__________[add] - para adicionar produto:\n\n__________[rm] - para remover produto:\n\n__________[list] - para listar os preços, quantidade de estoque e preço:\n");
+        inicio:
+
+        Console.WriteLine("\n\nOque você deseja:\n\n__________[add] - para adicionar produto:\n\n__________[rm] - para remover produto:\n\n__________[list] - para listar os preços, quantidade de estoque e preço:\n\n__________[quit] - para fechar o Software");
 
         Console.Write("Opção: ");
         respostaUser = Console.ReadLine();
 
-        
+
 
         switch(respostaUser){
 
@@ -30,12 +36,14 @@ class Estoque{
                 if (produto[N] == null)
                 {
                     produto[N] = Console.ReadLine();
+                    codigo[N] = N;
                 } else {
                     while (!(produto[N] == null)) 
                     {
                         N++;
                     }
                     produto[N] = Console.ReadLine();
+                    codigo[N] = N;
                 }
 
                 Console.Write("Quantidade de produto: ");
@@ -44,12 +52,14 @@ class Estoque{
                 if (quantidade[Q] == 0)
                 {
                     quantidade[Q] = int.Parse(Console.ReadLine());
+                    codigo[Q] = Q;
                 } else {
                     while (!(quantidade[Q] == 0))
                     {
                         Q++;
                     }
                     quantidade[Q] = int.Parse(Console.ReadLine());
+                    codigo[Q] = Q;
                 }
 
                 Console.Write("Preço do produto: ");
@@ -58,12 +68,14 @@ class Estoque{
                 if (preço[P] == 0)
                 {
                     preço[P] = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+                    codigo[P] = P;
                 } else {
                     while(!(preço[P] == 0))
                     {
                         P++;
                     }
                     preço[P] = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+                    codigo[P] = P;
                 }
 
                 Console.WriteLine("Quer adicionar mais algum produto: [S/N]");
@@ -74,23 +86,55 @@ class Estoque{
                     Console.Clear();
                     goto add;
                 } else {
-                    Console.WriteLine(produto[0]);
-                    Console.WriteLine(quantidade[0]);
-                    Console.WriteLine(preço[0]);
+                    Console.Clear();
+                    goto inicio;
+                }
+                    
+                
+            case "rm":
+                rm:
+                Console.Write("Qual o código do Produto que você deseja remover: ");
 
-                    Console.WriteLine(produto[1]);
-                    Console.WriteLine(quantidade[1]);
-                    Console.WriteLine(preço[1]);
+                int rm = int.Parse(Console.ReadLine());
+                
+                if (codigo[rm] != 0)
+                {
+                    produto[rm] = null;
+                    quantidade[rm] = 0;
+                    preço[rm] = 0;
+                } else {
+                    Console.WriteLine("Código inexistente. Quer tentar novamente: [S/N]");
+                    respostaUser = Console.ReadLine();
+
+                    if (respostaUser == "S" || respostaUser == "s")
+                    {
+                        Console.Clear();
+                        goto rm;
+                    } else {
+                        Console.Clear();
+                        goto inicio;
+                    }
                 }
                 break;
 
-            case "rm":
-                break;
-
             case "list":
+                Console.WriteLine("Todos os produtos, preços e estoque:");
+                if (produto[0] == null) 
+                {
+                    Console.WriteLine("Sua lista de produtos está vazia");
+
+                } else {
+                    for (int n = 0; produto[n] != null; n++)
+                    {
+                        Console.WriteLine("Produto: {0}", produto[n]);
+                        Console.WriteLine("Preço: {0}", preço[n]);
+                        Console.WriteLine("estoque: {0}", quantidade[n]);
+                    }
+                }
                 break;
 
-            default:
+
+            case "quit":
                 break;
 
         }
