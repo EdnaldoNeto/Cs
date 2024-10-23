59d7aed3a0ff9319ba8698ec67467f6ee2748765
Ednaldo_Neto <ednaldoramosneto@gmail.com>
2024-10-23 18:16:23 -0300

    up

--- Aulas/Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
+++ Aulas/Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d20de3180d4d656bb80a6823fdc605eace772334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53e5d090faa0deb578d8abac37bbad279b2c2b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
